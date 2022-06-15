37357ccccb8b07fb18f40d12f4252b4e2b1c14e7
seungbeom-han <hansb980522@gmail.com>
2022-06-15 18:29:38 +0900

    fix relative path error when downloading external tools

--- scripts/get_programs.sh
+++ scripts/get_programs.sh
@@ -6,7 +6,7 @@ wget -O $1/2.0.tar.gz -P $1 \
 https://github.com/wheaton5/souporcell/archive/refs/tags/2.0.tar.gz
 tar -xvf $1/strelka-2.9.10.centos6_x86_64.tar.bz2
 tar -xvzf $1/2.0.tar.gz
-cd $1/souporcell-2.0/troublet && cargo build --release
+cargo build --release --manifest-path $1/souporcell-2.0/troublet/Cargo.toml
 mv $1/souporcell-2.0/troublet/target/release/troublet $1/troublet
 ls $1/strelka-2.9.10.centos6_x86_64/bin/
 rm -r $1/souporcell-2.0 $1/2.0.tar.gz $1/strelka-2.9.10.centos6_x86_64.tar.bz2
